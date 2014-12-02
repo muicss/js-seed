122d7c20e235c52ba3314724eb3c9cf2071078c1
Andres Morey <andres@octopart.com>
2014-12-02 18:21:27 -0500

    Updated init.js

--- src/js/init.js
+++ src/js/init.js
@@ -1,10 +1,12 @@
 /**
- * main.js - example of file that imports a library
+ * init.js - example of file that imports a library
  *
  */
 var lib = require('./lib.js');
 
+// fire on window 'load' event
 lib.attachEvent(window, 'load', function() {
+  // fire on 'click' event
   lib.attachEvent(document.body, 'click', function(ev) {
     if (ev.target.tagName === 'BUTTON') alert('js-seeeeed!');
   });
